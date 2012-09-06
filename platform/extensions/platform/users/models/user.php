af490e1fac73ee792a7a4b1505421f014e4958cc
Daniel Petrie <dan.petrie@cartalyst.com>
2012-09-06 12:15:53 -0700

    fixing user formatting return on registration

--- platform/extensions/platform/users/models/user.php
+++ platform/extensions/platform/users/models/user.php
@@ -197,7 +197,7 @@ class User extends Crud
 
 					if (in_array('after', $events))
 					{
-						$result['id'] = $this->after_insert($result);
+						$result = $this->after_insert($result);
 					}
 
 					$user_id = (int) $result['id'];
