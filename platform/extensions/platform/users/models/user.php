9d039b6d58eae24e9b7f13e49ede0ad3ef96f274
Daniel Petrie <dan.petrie@cartalyst.com>
2012-09-12 16:30:47 -0700

    fixing user permission issue

--- platform/extensions/platform/users/models/user.php
+++ platform/extensions/platform/users/models/user.php
@@ -411,7 +411,7 @@ class User extends Crud
 		}
 
 		// if just updating permissions, remove rules
-		if (isset($data['id']) and isset($data['permissions']) and count($data) == 2)
+		if (isset($data['id']) and isset($data['permissions']))
 		{
 			$rules = array();
 		}
